804f8acc5374571b2dd307f01edd64a08989480f
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-17 23:45:42 +0300

    Summary

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-'use strict';
+const score0 = document.getElementById('score-0');
+const score1 = document.getElementById('score-1');
+const current0 = document.getElementById('current-0');
+const current1 = document.getElementById('current-1');
+const player0 = document.querySelector('.player-0');
+const player1 = document.querySelector('.player-1');
+const btnNew = document.querySelector('.btn-new');
+const btnRoll = document.querySelector('.btn-roll');
+const btnHold = document.querySelector('.btn-hold');
+const dice = document.querySelector('.dice');
